7c4986805e0d33705b8795dc0347c7114842dd83
Nicole Silvestrini Garrio <nsgarrio@gmail.com>
2024-10-05 19:25:06 -0300

    Class Pessoa

---
+++ Exercicios/Main.java
@@ -0,0 +1,44 @@
+package Exercicios;
+
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Pessoa P1 = new Pessoa("Nicole", "468.452.446-75", 20);
+        Pessoa P2 = new Pessoa("Letícia", "456.235.998-51", 13);
+        Pessoa P3 = new Pessoa("Nicole", "468.452.446-75", 20);
+
+        // System.out.printf("Pessoa 1 \nNome: %s \nCPF: %s \nIdade: %d", P1.getNome(),
+        // P1.getCpf(), P1.getIdade());
+        System.out.println("Lista de Pessoas: ");
+        System.out.println(P1.toString());
+        System.out.println(P2.toString());
+        System.out.println(P3.toString());
+
+        // P1.setIdade(21);
+        // System.out.printf("\n\nA nova idade de %s é %d\n\n", P1.getNome(),
+        // P1.getIdade());
+
+        var aaa = P1.equals(P2);
+        System.out.println("Resultado equals: " + aaa);
+
+        var bbb = P1.equals(P1);
+        System.out.println("Resultado equals: " + bbb);
+
+        HashSet<Pessoa> pessoas = new HashSet<>();
+        pessoas.add(P1);
+        pessoas.add(P2);
+        pessoas.add(P3);
+
+        System.out.println("(HashCode) Lista de Pessoas: ");
+        for (Pessoa p : pessoas) {
+            System.out.println(p);
+        }
+
+        Pessoa verificarPessoa = new Pessoa("Nicole", "468.452.446-75", 20);
+        System.out.println("O conjunto contém a Nicole? " + pessoas.contains(verificarPessoa));
+    }
+
+}
---
+++ Exercicios/Pessoa.java
@@ -0,0 +1,83 @@
+package Exercicios;
+
+import java.util.Objects;
+
+public class Pessoa {
+    private String nome, cpf;
+    private int idade;
+
+    private Boolean isValidNome(String nome) {
+        if (nome == null)
+            return false;
+
+        return true;
+    }
+
+    private Boolean isValidCpf(String cpf) {
+        if (cpf == null)
+            return false;
+
+        return true;
+
+    }
+
+    private Boolean isValidIdade(int idade) {
+        if (idade < 0 || idade > 120)
+            return false;
+
+        return true;
+
+    }
+
+    public Pessoa(String nome, String cpf, int idade) throws Exception {
+        if (!isValidNome(nome) || !isValidCpf(cpf) || !isValidIdade(idade))
+            throw new Exception("Dados inválidos!");
+
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    // public
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int novaidade) throws Exception {
+        if (!isValidIdade(novaidade))
+            throw new Exception("A idade informada não é válida");
+
+        this.idade = novaidade;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Dados Pessoais \nNome: " + this.nome + "\nCPF: " + this.cpf + "\nIdade: " + this.idade + "\n\n";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Pessoa))
+            return false;
+        Pessoa pessoa = (Pessoa) obj;
+        return idade == pessoa.idade && nome.equals(pessoa.nome) && cpf.equals(pessoa.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, idade, cpf);
+    }
+
+}
