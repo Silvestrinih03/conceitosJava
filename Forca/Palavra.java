95154fd0b2355aaaf5d963927c84d812bdaab415
Nicole Silvestrini Garrio <nsgarrio@gmail.com>
2024-11-05 11:09:52 -0300

    Classe Palavra quase pronta

--- Forca/Palavra.java
+++ Forca/Palavra.java
@@ -1,22 +1,31 @@
-public class Palavra implements Comparable<Palavra>
-{
+public class Palavra implements Comparable<Palavra> {
     private String texto;
 
-    public Palavra (String texto) throws Exception
-    {
-		// verifica se o texto recebido È nulo ou ent„o vazio,
-		// ou seja, sem nenhum caractere, lanÁando exceÁ„o.
-		// armazena o texto recebido em this.texto.
+    public Palavra(String texto) throws Exception {
+        // verifica se o texto recebido √© nulo ou ent√£o vazio,
+        // ou seja, sem nenhum caractere, lan√ßando exce√ß√£o.
+        // armazena o texto recebido em this.texto.
+        if (texto == null || texto.isEmpty())
+            throw new Exception("Palavra inv√°lida!");
+        this.texto = texto;
     }
 
-    public int getQuantidade (char letra)
-    {
+    public int getQuantidade(char letra) {
         // percorre o String this.texto, conta e retorna
         // quantas letras existem nele iguais a letra fornecida
+        char[] caracteres = this.texto.toCharArray();
+        int cont = 0;
+
+        // percorre o array e conta quantas vezes a letra aparece
+        for (int i = 0; i < caracteres.length; i++) {
+            if (caracteres[i] == letra) {
+                cont++;
+            }
+        }
+        return cont;
     }
 
-    public int getPosicaoDaIezimaOcorrencia (int i, char letra) throws Exception
-    {
+    public int getPosicaoDaIezimaOcorrencia(int i, char letra) throws Exception {
         // se i==0, retorna a posicao em que ocorre a primeira
         // aparicao de letra fornecida em this.texto;
         // se i==1, retorna a posicao em que ocorre a segunda
@@ -24,33 +33,49 @@ public class Palavra implements Comparable<Palavra>
         // se i==2, retorna a posicao em que ocorre a terceira
         // aparicao de letra fornecida em this.texto;
         // e assim por diante.
-        // lanÁar excecao caso nao encontre em this.texto
-        // a IÈzima apariÁ„o da letra fornecida.
+        // lan√ßar excecao caso nao encontre em this.texto
+        // a I√©zima apari√ß√£o da letra fornecida.
+
     }
 
-    public int getTamanho ()
-    {
+    public int getTamanho() {
         return this.texto.length();
     }
 
-    public String toString ()
-    {
+    public String toString() {
         return this.texto;
     }
 
-    public boolean equals (Object obj)
-    {
-        // verificar se this e obj possuem o mesmo conte˙do, retornando
+    public boolean equals(Object obj) {
+        // verificar se this e obj possuem o mesmo conte√∫do, retornando
         // true no caso afirmativo ou false no caso negativo
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() != obj.getClass())
+            return false;
+
+        Palavra palavra = (Palavra) obj;
+        return this.texto.equals(palavra.texto);
     }
 
-    public int hashCode ()
-    {
+    public int hashCode() {
         // calcular e retornar o hashcode de this
+        int ret = 1;
+
+        for (int i = 0; i < this.texto.length(); i++) {
+            ret = ret * 13 + this.texto.hashCode();
+        }
+
+        if (ret < 0)
+            ret = -ret;
+        return ret;
+
+        // return this.texto.hashCode();
     }
 
-    public int compareTo (Palavra palavra)
-    {
+    public int compareTo(Palavra palavra) {
         return this.texto.compareTo(palavra.texto);
     }
 }
