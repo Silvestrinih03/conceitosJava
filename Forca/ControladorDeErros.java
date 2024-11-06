d6c63830b0c11fbe33bbba3eca0fd0d797bc2bb3
Nicole Silvestrini Garrio <nsgarrio@gmail.com>
2024-11-06 10:50:53 -0300

    Controlador de erros pronto

--- Forca/ControladorDeErros.java
+++ Forca/ControladorDeErros.java
@@ -1,64 +1,96 @@
-public class ControladorDeErros implements Cloneable
-{
-    private int qtdMax, qtdErr=0;
+public class ControladorDeErros implements Cloneable {
+    private int qtdMax, qtdErr = 0;
 
-    public ControladorDeErros (int qtdMax) throws Exception
-    {
-        // verifica se qtdMax fornecida n„o È positiva, lanÁando
-        // uma exceÁ„o.
+    public ControladorDeErros(int qtdMax) throws Exception {
+        // verifica se qtdMax fornecida n√£o √© positiva, lan√ßando
+        // uma exce√ß√£o.
         // armazena qtdMax fornecida em this.qtdMax.
-        if (qtdMax<0) throw new Exception ("Quantidade invalida");
+        if (qtdMax < 0)
+            throw new Exception("Quantidade invalida");
 
-        this.qtdMax=qtdMax;
+        this.qtdMax = qtdMax;
     }
 
-    public void registreUmErro () throws Exception
-    {
-        // verifica se this.qtdErr ja È igual a this.qtdMax,
-        // lanÁando excecao em caso positivo ou
+    public void registreUmErro() throws Exception {
+        // verifica se this.qtdErr ja √© igual a this.qtdMax,
+        // lan√ßando excecao em caso positivo ou
         // incrementando this.qtdErr em caso negativo
-        if (this.qtdErr==this.qtdMax) throw new Exception ("Errou alem do maximo");
+        if (this.qtdErr == this.qtdMax)
+            throw new Exception("Errou alem do maximo");
 
         this.qtdErr++;
     }
 
-    public boolean isAtingidoMaximoDeErros  ()
-    {
+    public boolean isAtingidoMaximoDeErros() {
         // returna true se this.qtdErr for igual a this.qtdMax,
-        // ou ent„o false, caso contrario.
+        // ou ent√£o false, caso contrario.
         /*
-        if (this.qtdErr==this.qtdMax)
-            return true;
-        else
-            return false;
-        */
+         * if (this.qtdErr==this.qtdMax)
+         * return true;
+         * else
+         * return false;
+         */
 
-        return this.qtdErr==this.qtdMax;
+        return this.qtdErr == this.qtdMax;
     }
 
-    public String toString ()
-    {
-        return this.qtdErr + "/" + this.qtdMax;
+    public String toString() {
+        return "Erros: " + this.qtdErr + "/" + this.qtdMax;
     }
 
-    public boolean equals (Object obj)
-    {
-        // verificar se this e obj possuem o mesmo conte˙do, retornando
+    public boolean equals(Object obj) {
+        // verificar se this e obj possuem o mesmo conte√∫do, retornando
         // true no caso afirmativo ou false no caso negativo
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() != obj.getClass())
+            return false;
+
+        ControladorDeErros erros = (ControladorDeErros) obj;
+
+        if (erros.qtdErr != this.qtdErr)
+            return false;
+        if (erros.qtdMax != this.qtdMax)
+            return false;
+
+        return true;
     }
 
-    public int hashCode ()
-    {
+    public int hashCode() {
         // calcular e retornar o hashcode de this
+        int ret = 1;
+
+        ret = ret * 31 + this.qtdErr;
+        ret = ret * 13 + this.qtdMax;
+
+        if (ret < 0)
+            ret = -ret;
+        return ret;
     }
 
-    public ControladorDeErros (ControladorDeErros c) throws Exception // construtor de cÛpia
+    public ControladorDeErros(ControladorDeErros c) throws Exception // construtor de c√≥pia
     {
         // copiar c.qtdMax e c.qtdErr, respectivamente em, this.qtdMax e this.qtdErr
+        if (c == null)
+            throw new Exception("Modelo ausente!");
+
+        this.qtdErr = c.qtdErr;
+        this.qtdMax = c.qtdMax;
+
     }
 
-    public Object clone ()
-    {
-        // returnar uma cÛpia de this
+    public Object clone() {
+        // returnar uma c√≥pia de this
+        ControladorDeErros ret = null;
+
+        try {
+            ret = new ControladorDeErros(this);
+        } catch (Exception erro) {
+            throw new RuntimeException("Erro ao clonar o objeto: " + erro.getMessage(), erro);
+        }
+
+        return ret;
     }
 }
